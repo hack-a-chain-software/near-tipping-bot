b12a36b87e41c06cfe3887b53af5b191a748de6d
Eduardo <eduardogbg@gmail.com>
2022-12-02 12:12:47 -0300

    chore(bot): rename bot package

--- packages/bot/index.js
+++ packages/bot/index.js
@@ -9,6 +9,8 @@ const client = new Client({
 // creating a new collection for the commands
 ["commands", "aliases"].forEach((file) => (client[file] = new Collection()));
 // passing the client to be used in events and commands
-["commands", "events"].forEach((file) => require(`./src/handlers/${file}`)(client));
+["commands", "events"].forEach((file) =>
+  require(`./src/handlers/${file}`)(client)
+);
 
 client.login(process.env.BOT_TOKEN);
