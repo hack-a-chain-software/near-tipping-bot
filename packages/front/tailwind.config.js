d2329f47be4bb02af10a4b6a96996deb82d39287
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-12-02 10:10:53 -0300

    feat(front): add button gradient

--- packages/front/tailwind.config.js
+++ packages/front/tailwind.config.js
@@ -3,24 +3,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-          azoxo: "#6D71FF",
-          black: "#333333",
-          pink: "#FC89F7",
-          blue_button: "#7983FF",
-          graphite: "#424250",
-          purple: "#AB80FF",
-          grey: "#3D3D3D"
+        azoxo: "#6D71FF",
+        black: "#333333",
+        pink: "#FC89F7",
+        blue_button: "#7983FF",
+        graphite: "#424250",
+        purple: "#AB80FF",
+        grey: "#3D3D3D",
       },
-
       fontSize: {
-          fS32px : '32px'
+        fS32px: "32px",
       },
       backgroundImage: {
-        "lpbg": "url('/images/bg_landing_page_1x.jpg')",
-
+        lpbg: "url('/images/bg_landing_page_1x.jpg')",
+        button: "linear-gradient(270deg, #7983FF 47.22%, #FC89F7 100%)",
       },
-
-
     },
   },
   plugins: [],
