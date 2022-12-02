7c06c8c6811981b4dc06f2fd2110b31897a4b2d5
Eduardo <eduardogbg@gmail.com>
2022-12-02 12:11:54 -0300

    feat(db): add migrations framework & containerize

--- packages/db/migrations/0005_feedbacks.sql
+++
@@ -1,8 +0,0 @@
-create table feedbacks (
-    id uuid primary key default gen_random_uuid(),
-    server_id bigint not null,
-    user_id bigint not null,
-    username text not null,
-    message text not null,
-    submitted_at timestamptz default now()
-);
