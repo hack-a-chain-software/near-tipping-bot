d343246ba23e95289a48b985dfb151e95019dbd2
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-12-14 17:30:56 -0300

    feat(front): add requested changes on page

--- packages/front/src/components/register-token/button.tsx
+++ packages/front/src/components/register-token/button.tsx
@@ -3,9 +3,16 @@ export const ButtonRegisterToken = () => {
     <button
       className="text-white flex rounded-full py-3 px-2.5 justify-between w-1/8 text-xl  inline-flex items-center bg-bgbutton bottom-0 "
       type="button"
-      onClick={() => window.open("https://discord.gg/kfJQa2Ws")}
+      onClick={() =>
+        window.open(
+          "https://discord.com/api/oauth2/authorize?client_id=1051851105300123668&permissions=2147503104&scope=bot"
+        )
+      }
     >
-      <span className=" font-medium px-4 text-xl"> Click here to request </span>
+      <span className=" font-medium px-4 text-xl">
+        {" "}
+        Get the bot on your server{" "}
+      </span>
     </button>
   );
 };
