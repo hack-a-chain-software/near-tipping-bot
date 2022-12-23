d5323099241cc667ac1624d6b109c6646924a837
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-12-22 22:54:59 -0300

    Update index.tsx

--- packages/front/src/pages/index.tsx
+++ packages/front/src/pages/index.tsx
@@ -57,72 +57,91 @@ function Index() {
       </Section>
 
       <Divider>
-        <Section id="how-to-use" className="pt-24 pb-56">
+        <Section id="tutorial" className="pt-24 pb-56">
           <div className="flex justify-center mb-16">
             <Title>
-              How to <span className="text-pink my-4">use</span>
+              Bot <HighlightText>tutorial</HighlightText>
             </Title>
           </div>
 
-          <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(210px,250px))] gap-12 justify-center">
-            {tutorialItems.map((step, id) => (
-              <TutorialCard
-                {...step}
-                index={id}
-                key={`how-to-use-peter-bot-step-${id}`}
-              />
-            ))}
+          <div>
+            <iframe
+              id="ytplayer"
+              width="100%"
+              loading="lazy"
+              frameBorder="0"
+              className="aspect-video"
+              style={{
+                border: "2px solid white",
+                borderRadius: "8px",
+              }}
+              src="https://www.youtube.com/embed/FBPkdyliV1g?modestbranding=1&autohide=1&showinfo=0&controls=1"
+            />
           </div>
         </Section>
       </Divider>
 
-      <Section id="available-tokens" className="pt-24 pb-32">
-        <div className="text-center mb-16">
+      <Section id="how-to-use" className="pt-24 pb-56">
+        <div className="flex justify-center mb-16">
           <Title>
-            Our <HighlightText>available tokens</HighlightText>
+            How to <HighlightText>use</HighlightText>
           </Title>
         </div>
 
-        <AvaiableTokens />
+        <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(210px,250px))] gap-12 justify-center">
+          {tutorialItems.map((step, id) => (
+            <TutorialCard
+              {...step}
+              index={id}
+              key={`how-to-use-peter-bot-step-${id}`}
+            />
+          ))}
+        </div>
       </Section>
 
       <Divider>
-        <Section id="register-token" className="pt-32 pb-60">
-          <div className="text-center">
+        <Section id="available-tokens" className="pt-24 pb-32">
+          <div className="text-center mb-16">
             <Title>
-              Didn’t find the <HighlightText>token you need?</HighlightText>
+              Our <HighlightText>available tokens</HighlightText>
             </Title>
           </div>
 
-          <div className="flex items-center justify-center ml-[-211px]">
-            <img
-              loading="lazy"
-              src="/images/tipping_bot.png"
-              alt="Tipping bot"
-            />
-
-            <div className="flex flex-col pl-8">
-              <p className="text-center pb-12 font-normal text-xl text-white">
-                Make a request to add a new token{" "}
-                <br className="hidden lg:block"></br>
-                by using <span className="text-pink">/newtoken</span> command
-              </p>
-
-              <Button
-                onClick={() =>
-                  window.open(
-                    "https://discord.com/api/oauth2/authorize?client_id=1051851105300123668&permissions=2147503104&scope=bot"
-                  )
-                }
-              >
-                <span>Get the bot on your server </span>
-
-                <DiscordIcon />
-              </Button>
-            </div>
-          </div>
+          <AvaiableTokens />
         </Section>
       </Divider>
+
+      <Section id="register-token" className="pt-32 pb-60">
+        <div className="text-center">
+          <Title>
+            Didn’t find the <HighlightText>token you need?</HighlightText>
+          </Title>
+        </div>
+
+        <div className="flex items-center justify-center ml-[-211px]">
+          <img loading="lazy" src="/images/tipping_bot.png" alt="Tipping bot" />
+
+          <div className="flex flex-col pl-8">
+            <p className="text-center pb-12 font-normal text-xl text-white">
+              Make a request to add a new token{" "}
+              <br className="hidden lg:block"></br>
+              by using <span className="text-pink">/newtoken</span> command
+            </p>
+
+            <Button
+              onClick={() =>
+                window.open(
+                  "https://discord.com/api/oauth2/authorize?client_id=1051851105300123668&permissions=2147503104&scope=bot"
+                )
+              }
+            >
+              <span>Get the bot on your server </span>
+
+              <DiscordIcon />
+            </Button>
+          </div>
+        </div>
+      </Section>
 
       <Footer />
     </main>
