14002bd084f61a1cc999ca719c31cbd1cde86551
Alexandre <alexandre01393@gmail.com>
2023-01-30 10:54:06 -0300

    fix(bot): changing number of decimals

--- packages/bot/src/utils/formatDecimals.js
+++ packages/bot/src/utils/formatDecimals.js
@@ -1,7 +1,7 @@
 const { default: Big } = require("big.js");
 
 const formatDecimals = (amount, decimals) => {
-  return Big(amount).div(Big(10).pow(decimals)).toFixed(2);
+  return Big(amount).div(Big(10).pow(decimals)).toFixed(8);
 };
 
 module.exports = { formatDecimals };
